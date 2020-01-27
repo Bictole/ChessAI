64b1b8c32477e998ed06fb72cbb6606733835e76
jeannemorin <46577907+jeannemorin@users.noreply.github.com>
2020-01-27 05:57:56 +0100

    Class game piece and player AND Qleraning stuff

---
+++ Board.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 22 21:44:37 2020
+
+@author: morin
+"""
+
+class piece:
+    
+    def __init__(self,name,value,is_white):
+        self.name = name
+        self.value = value
+        self.is_white = is_white
+    
+class player:
+    
+    def __init__(self,is_white):
+        self.is_white = is_white
+        self.piece = []
+        for i in range(0,8):
+            p = piece("pawn",1,is_white)
+            self.piece.append(p)
+        for i in range(0,2):
+            p = piece("rook",5,is_white)
+            self.piece.append(p)
+        for i in range(0,2):
+            p = piece("knight",3,is_white)
+            self.piece.append(p)
+        for i in range(0,2):
+            p = piece("bishop",3,is_white)
+            self.piece.append(p)
+        p = piece("queen",9,is_white)
+        self.piece.append(p)
+        p = piece("king",15,is_white)
+        self.piece.append(p)
+        
+        
+        
+class game:
+    
+    board = [0]*64
+    
+    def __init__(self):
+        for i in range(0,8):
+            letter = chr(97+i)
+            for j in range(0,8):
+                self.board[i*8+j]= letter+str(j)
+        
+        self.P1 = player(True)
+        self.P2 = player(False)
+        self.turn = 0
+        
+
+
+def display_board(board):
+    print(" ____ ____ ____ ____ ____ ____ ____ ____")
+    for i in range(0,8):
+        for j in range(0,8):
+            print("|",board[i*8+j],end=" ")
+        print("|")
+    
+    print(" ____ ____ ____ ____ ____ ____ ____ ____ ")
+        
+def test():
+    c = game()
+    display_board(c.board)
---
+++ QLearningpy.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 22 19:43:01 2020
+
+@author: morin
+EASY EXAMPLE
+"""
+import random
+import time
+
+class stat:
+    
+    reward = 0
+    proba = []
+    quality = []
+    
+    def __init__(self, reward, proba, quality):
+        
+        if(len(proba) != len(quality)):
+            return False
+        
+        self.proba = proba
+        self.quality = quality
+        self.reward = reward
+
+
+def new_graph() :
+    Graph = []
+    for i in range(0,4):
+        L = [0.25,0.25,0.25,0.25]
+        K = [0]*4
+        L[i] = 0;
+        L[(i%3+1)] +=0.25
+        s = stat(random.randint(-5,5),L,K)
+        Graph.append(s)
+    
+    Graph[3].reward = 200
+    
+    return Graph
+
+
+
+def max_Q(s,nb):
+    maxi = nb%3+1
+    for i in range (len(s.quality)):
+        
+        if(s.quality[i]>s.quality[maxi]):
+            maxi = i
+              
+    return maxi
+        
+        
+
+def Qlearning(learning_rate, episode):
+    G = new_graph()
+    for i in range(0,episode):
+        l = 1
+        e = 1
+        current = 0
+        for i in range(0,4):
+            s = G[current]
+            nb = random.randint(0,1)
+            if(nb<e):
+                a = random.randint(0,3)
+            else:
+                a = max_Q(s,current)
+            
+            
+            s_bis = G[a] #action 1 leads to state 1, action 2 to state 2, ...
+            s.quality[a] = l*(s_bis.reward + 0.4*max_Q(s_bis,a) + (1-l)*s.quality[a])
+            l *=0.99
+            e*=0.99
+            print("S",current,"---->","S",a)
+            if(s_bis == G[3]):
+                break
+            
+            current = a
+            
+            
+    display(G)
+    #time.sleep(10)  
+    
+def display(G):
+    
+    for i in range (len(G)):
+        print("S",i)
+        current = G[i]
+        for stat in range(len(G)):
+            if(stat != i):
+                print("----> S",stat, "   P =",current.proba[stat], "   Q =",current.quality[stat])
+                print("\n\n")
+                
+                
+            
+          
+        
