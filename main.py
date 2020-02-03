0f5671cb1743e43a2f2489df9b33155f0e445a3a
Victor SIMONIN <victor.simonin@epita.fr>
2020-02-03 17:45:47 +0530

    piece mangé ok

--- main.py
+++ main.py
@@ -15,19 +15,21 @@ def main():
     white = game.P1
     current = white
 
+
     mat=False
     pat = False
     while(not mat and not pat):
         Board.display_board(game.board)
         position = []
         piece = None
+        eaten_piece = None
 
         #Prends la nom de la pièce que l'on veut bouger. Recommence tant que le nom n'est pas valide
         while position == []:
             piece_name = input("Which piece you wanna move?")
             for p in current.pieces:
                 if(p.name == piece_name):
-                    position = p.possible_moves(game.board)
+                    position = p.possible_moves(game)
                     piece = p
                     break
             if(position == []):
@@ -52,22 +54,24 @@ def main():
                 print("This position doesn't exist, please enter a good one !")
                 for e in position:
                     print(e)
+        #find out if there is any pieceto eat
+        if(game.board[indexpos]):
+            eaten_piece = game.board[indexpos]
 
+        #Move the current piece
         game.board[piece.position] = None
         piece.position = indexpos
         game.board[piece.position] = piece
-
-
-
-        #check mat
-        #check pat
-
 
         if(current == white):
             current = black
         else:
             current = white
+        if(eaten_piece):
+            current.pieces.remove(eaten_piece)
 
+        # check mat
+        # check pat
 
 
 main()
