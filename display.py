61d20f4fd7b510905dc7bb88d170e22f3134d270
Victor SIMONIN <victor.simonin@epita.fr>
2020-02-03 15:59:12 +0530

    last'

--- display.py
+++ display.py
@@ -1,5 +1,6 @@
 #coding:utf-8
 import pygame
+import time
 
 pygame.init()
 window_surface = pygame.display.set_mode((600, 600), pygame.RESIZABLE)
@@ -9,9 +10,14 @@ white = (255, 255, 255)
 window_surface.fill(white)
 pygame.display.flip()
 
-chess_image = pygame.image.load("Chessboard.png")
+chess_image = pygame.image.load("Images/Chessboard.png")
 chess_image = pygame.transform.scale(chess_image, (600,600))
 chess_image.convert_alpha()
+
+#song = pygame.mixer.Sound("Images/PianoContre.mp3")
+#song.play(100, 0, 5000)
+
+clock = pygame.time.Clock()
 
 launched = True
 
@@ -22,5 +28,6 @@ while launched:
 
     window_surface.blit(chess_image, [0, 0])
     pygame.display.flip()
+    clock.tick()
 
 pygame.quit()
