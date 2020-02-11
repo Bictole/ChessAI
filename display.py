67cc403ac6a957809308d6e1d3702f16f4ec58f5
Victor SIMONIN <victor.simonin@epita.fr>
2020-02-11 23:19:25 +0530

    display well

--- display.py
+++ display.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.locals import *
 import os
 import time
+import Board
 
 pygame.init()
 window_surface = pygame.display.set_mode((800, 800), pygame.RESIZABLE)
@@ -99,13 +100,27 @@ bking_image = pygame.transform.scale(bking_image, (80,80))
 #song = pygame.mixer.Sound("Images/PianoContre.mp3")
 #song.play(100, 0, 5000)
 
+greenpoint_image = pygame.image.load("Images/GreenPoint.png").convert_alpha()
+greenpoint_image = pygame.transform.scale(greenpoint_image, (50, 50))
+
 position_tab = [60, 145, 230, 315, 400, 485, 570, 655]
 
 clock = pygame.time.Clock()
 
+#Init the variables
+game = Board.game()
+black = game.P2
+white = game.P1
+current = white
+
+mat=False
+pat = False
 launched = True
 
-while launched:
+piece = None
+eaten_piece = None
+moves = []
+while launched and not mat and not pat:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             launched = False
@@ -124,53 +139,147 @@ while launched:
                     if stepy > 0 and stepy < miny:
                         miny = stepy
                         indexy = i
-                print("x -> ", position_tab[indexx], "  y -> ", position_tab[indexy], end='\n')
 
+                posx = position_tab[indexx]
+                posy = position_tab[indexy]
 
+                for p in current.pieces:
+                    if p.position == indexy*8 + indexx:
+                        piece = p
+                        break
 
+        if event.type == pygame.MOUSEBUTTONUP and piece:
+            minx = 1000
+            miny = 1000
+            indexx = -1
+            indexy = -1
+            for i in range(len(position_tab)):
+                stepx = event.pos[0] - position_tab[i]
+                stepy = event.pos[1] - position_tab[i]
+                if stepx > 0 and stepx < minx:
+                    minx = stepx
+                    indexx = i
+                if stepy > 0 and stepy < miny:
+                    miny = stepy
+                    indexy = i
+
+            posa = position_tab[indexx]
+            posb = position_tab[indexy]
+
+            moves = piece.possible_moves(game)
+            boolean_move = False
+            moveindex = -1
+            for i in range(len(moves)):
+                coor = [60 + (moves[i] % 8) * 85, 60 + (moves[i] // 8) * 85]
+                if coor == [posa, posb]:
+                    boolean_move = True
+                    moveindex = i
+                    break
+
+            if boolean_move:
+                moveposition = moves[moveindex]
+
+                #Find out if there is any piece to eat
+                if (game.board[moveposition]):
+                    eaten_piece = game.board[moveposition]
+
+                # Move the current piece
+                game.board[piece.position] = None
+                piece.position = moveposition
+                game.board[piece.position] = piece
+
+                if (current == white):
+                    current = black
+                else:
+                    current = white
+                if (eaten_piece):
+                    current.pieces.remove(eaten_piece)
+                    eaten_piece = None
+                piece = None
+
+                # check mat
+                if (game.P1.pieces == [] or game.P2.pieces == []):
+                    mat = True
+                # check pat
 
     window_surface.blit(board_image, [0, 0])
 
+    for e in black.pieces:
+        if e.name[0] == "P":
+            window_surface.blit(bpawn1_image, [60 + (e.position%8)*85, 60 + (e.position//8)*85])
+        if e.name[0] == "C":
+            window_surface.blit(bknight1_image, [60 + (e.position%8)*85, 60 + (e.position//8)*85])
+        if e.name[0] == "B":
+            window_surface.blit(bbishop1_image, [60 + (e.position%8)*85, 60 + (e.position//8)*85])
+        if e.name[0] == "Q":
+            window_surface.blit(bqueen_image, [60 + (e.position%8)*85, 60 + (e.position//8)*85])
+        if e.name[0] == "K":
+            window_surface.blit(bking_image, [60 + (e.position%8)*85, 60 + (e.position//8)*85])
+        if e.name[0] == "R":
+            window_surface.blit(brook1_image, [60 + (e.position%8)*85, 60 + (e.position//8)*85])
+
+    for e in white.pieces:
+        if e.name[0] == "P":
+            window_surface.blit(wpawn1_image, [60 + (e.position%8)*85, 60 + (e.position//8)*85])
+        if e.name[0] == "C":
+            window_surface.blit(wknight1_image, [60 + (e.position%8)*85, 60 + (e.position//8)*85])
+        if e.name[0] == "B":
+            window_surface.blit(wbishop1_image, [60 + (e.position%8)*85, 60 + (e.position//8)*85])
+        if e.name[0] == "Q":
+            window_surface.blit(wqueen_image, [60 + (e.position%8)*85, 60 + (e.position//8)*85])
+        if e.name[0] == "K":
+            window_surface.blit(wking_image, [60 + (e.position%8)*85, 60 + (e.position//8)*85])
+        if e.name[0] == "R":
+            window_surface.blit(wrook1_image, [60 + (e.position%8)*85, 60 + (e.position//8)*85])
+
+    '''"#displaying the black team
+    window_surface.blit(brook1_image, [60, 60])
+    window_surface.blit(bknight1_image, [145, 60])
+    window_surface.blit(bbishop1_image, [230, 60])
+    window_surface.blit(bking_image, [315, 60])
+    window_surface.blit(bqueen_image, [400, 60])
+    window_surface.blit(bbishop2_image, [485, 60])
+    window_surface.blit(bknight2_image, [570, 60])
+    window_surface.blit(brook2_image, [655, 60])
+
+    window_surface.blit(bpawn1_image, [60, 145])
+    window_surface.blit(bpawn2_image, [145, 145])
+    window_surface.blit(bpawn3_image, [230, 145])
+    window_surface.blit(bpawn4_image, [315, 145])
+    window_surface.blit(bpawn5_image, [400, 145])
+    window_surface.blit(bpawn6_image, [485, 145])
+    window_surface.blit(bpawn7_image, [570, 145])
+    window_surface.blit(bpawn8_image, [655, 145])
+
     #displaying the white team
-    window_surface.blit(wrook1_image, [60, 60])
-    window_surface.blit(wknight1_image, [145, 60])
-    window_surface.blit(wbishop1_image, [230, 60])
-    window_surface.blit(wking_image, [315, 60])
-    window_surface.blit(wqueen_image, [400, 60])
-    window_surface.blit(wbishop2_image, [485, 60])
-    window_surface.blit(wknight2_image, [570, 60])
-    window_surface.blit(wrook2_image, [655, 60])
+    window_surface.blit(wrook1_image, [60, 655])
+    window_surface.blit(wknight1_image, [145, 655])
+    window_surface.blit(wbishop1_image, [230, 655])
+    window_surface.blit(wking_image, [315, 655])
+    window_surface.blit(wqueen_image, [400, 655])
+    window_surface.blit(wbishop2_image, [485, 655])
+    window_surface.blit(wknight2_image, [570, 655])
+    window_surface.blit(wrook2_image, [655, 655])
 
-    window_surface.blit(wpawn1_image, [60, 145])
-    window_surface.blit(wpawn2_image, [145, 145])
-    window_surface.blit(wpawn3_image, [230, 145])
-    window_surface.blit(wpawn4_image, [315, 145])
-    window_surface.blit(wpawn5_image, [400, 145])
-    window_surface.blit(wpawn6_image, [485, 145])
-    window_surface.blit(wpawn7_image, [570, 145])
-    window_surface.blit(wpawn8_image, [655, 145])
+    window_surface.blit(wpawn1_image, [60, 570])
+    window_surface.blit(wpawn2_image, [145, 570])
+    window_surface.blit(wpawn3_image, [230, 570])
+    window_surface.blit(wpawn4_image, [315, 570])
+    window_surface.blit(wpawn5_image, [400, 570])
+    window_surface.blit(wpawn6_image, [485, 570])
+    window_surface.blit(wpawn7_image, [570, 570])
+    window_surface.blit(wpawn8_image, [655, 570])'''
 
-    #displaying the black team
-    window_surface.blit(brook1_image, [60, 655])
-    window_surface.blit(bknight1_image, [145, 655])
-    window_surface.blit(bbishop1_image, [230, 655])
-    window_surface.blit(bking_image, [315, 655])
-    window_surface.blit(bqueen_image, [400, 655])
-    window_surface.blit(bbishop2_image, [485, 655])
-    window_surface.blit(bknight2_image, [570, 655])
-    window_surface.blit(brook2_image, [655, 655])
-
-    window_surface.blit(bpawn1_image, [60, 570])
-    window_surface.blit(bpawn2_image, [145, 570])
-    window_surface.blit(bpawn3_image, [230, 570])
-    window_surface.blit(bpawn4_image, [315, 570])
-    window_surface.blit(bpawn5_image, [400, 570])
-    window_surface.blit(bpawn6_image, [485, 570])
-    window_surface.blit(bpawn7_image, [570, 570])
-    window_surface.blit(bpawn8_image, [655, 570])
-
+    if piece:
+        #print(piece.name)
+        moves = piece.possible_moves(game)
+        for e in moves:
+            coor = [75 + (e%8)*85, 75 + (e//8)*85]
+            window_surface.blit(greenpoint_image, coor)
 
     pygame.display.flip()
     clock.tick()
+
+
 
 pygame.quit()
